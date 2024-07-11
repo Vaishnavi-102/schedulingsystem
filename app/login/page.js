64b42e167fa435a83c2d92bcc1b9b3327c022058
Srikanth Cheruku <wpsrikanth@gmail.com>
2024-07-11 11:19:59 +0530

    login & singup

--- app/login/page.js
+++ app/login/page.js
@@ -1,45 +1,118 @@
-import Header from "@/components/Header";
-import supabase from "@/utils/supabase";
-import { Auth } from "@supabase/auth-ui-react";
+'use client';
 
-import { PROJECT_NAME } from "@/utils/globals";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+export default function LoginPage(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-import { Input } from "@/components/ui/input";
 
-const Login = () => {
-  console.log(supabase);
-  return (
-    <div>
-      <Header />
-      <div className="">
-        <Card className="max-w-lg w-full rounded-lg mx-auto">
-          <CardHeader>
-            <CardTitle>Login to {PROJECT_NAME}</CardTitle>
-            <CardDescription>Login with email</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form className="flex flex-col gap-2">
-              <Input type="email" placeholder="someone@gmail.com" />
-              <Input type="password" placeholder="********" /> 
-            </form>
-          </CardContent>
-          <CardFooter className="flex items-center gap-2">
-            <button className="p-2 bg-blue-500 text-white rounded-lg">Login</button>
-            <button className="p-2 bg-blue-500 text-white rounded-md">Sign up</button>
-          </CardFooter>
-        </Card>
-      </div>
-    </div>
-  );
-};
+    const supabase = createClientComponentClient();
 
-export default Login;
+    useEffect(() => {
+        async function getUser(){
+            const {data: {user}} = await supabase.auth.getUser()
+            setUser(user)
+            setLoading(false)
+        }
+
+        getUser();
+    }, [supabase.auth])
+
+
+    const handleSignUp = async () => {
+        const res = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+                emailRedirectTo: `${location.origin}/auth/callback`
+            }
+        })
+        setUser(res.data.user)
+        router.refresh();
+        setEmail('')
+        setPassword('')
+    }
+
+    const handleSignIn = async () => {
+        const res = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        setUser(res.data.user)
+        router.refresh();
+        setEmail('')
+        setPassword('')
+    }
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut();
+        router.refresh();
+        setUser(null)
+    }
+
+    console.log({loading, user})
+
+    if (loading){
+        return <h1>loading..</h1>
+    }
+
+    if (user){
+        return (
+            <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+            <div className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-md w-96 text-center">
+                <h1 className="mb-4 text-xl font-bold text-gray-700 dark:text-gray-300">
+                    You're already logged in
+                </h1>
+                <button 
+                    onClick={handleLogout}
+                    className="w-full p-3 rounded-md bg-red-500 text-white hover:bg-red-600 focus:outline-none"
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
+        )
+    }
+
+    return (
+        <main className="h-screen flex items-center justify-center bg-gray-800 p-6">
+        <div className="bg-gray-900 p-8 rounded-lg shadow-md w-96">
+        <input 
+            type="email" 
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="mb-4 w-full p-3 rounded-md border border-gray-700 bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:border-blue-500"
+        />
+        <input 
+            type="password" 
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="mb-4 w-full p-3 rounded-md border border-gray-700 bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:border-blue-500"
+        />
+        <button 
+            onClick={handleSignUp}
+            className="w-full mb-2 p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none"
+        >
+            Sign Up
+        </button>
+        <button 
+            onClick={handleSignIn}
+            className="w-full p-3 rounded-md bg-gray-700 text-white hover:bg-gray-600 focus:outline-none"
+        >
+            Sign In
+        </button>
+        </div>
+        </main>
+    )
+
+}
